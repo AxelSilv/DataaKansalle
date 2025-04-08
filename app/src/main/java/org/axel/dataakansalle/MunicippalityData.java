70e3110584c166cd16400e20d4a248d9ae3da530
bhankai <kailash_4@icloud.com>
2025-04-08 14:49:02 +0300

    Class added

---
+++ app/src/main/java/org/axel/dataakansalle/MunicippalityData.java
@@ -0,0 +1,10 @@
+package org.axel.dataakansalle;
+
+public class MunicippalityData {
+
+    String municipality;
+
+    public MunicippalityData(String municipality){
+        this.municipality = municipality;
+    }
+}
