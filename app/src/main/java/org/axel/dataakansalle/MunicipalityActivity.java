e2588b8f288372ab805956d39a668715b144784e
bhankai <kailash_4@icloud.com>
2025-04-15 15:14:25 +0300

    TabLayout luotu fragmenteille

--- app/src/main/java/org/axel/dataakansalle/MunicipalityActivity.java
+++ app/src/main/java/org/axel/dataakansalle/MunicipalityActivity.java
@@ -7,6 +7,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MunicipalityActivity extends AppCompatActivity {
 
@@ -15,6 +18,35 @@ public class MunicipalityActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_municipality);
+
+        TabLayout tabLayout = findViewById(R.id.tabArea);
+        ViewPager2 fragmentArea = findViewById(R.id.fragmentArea);
+        MunicipalityPagerAdapter tabPagerAdapter = new MunicipalityPagerAdapter(this);
+        fragmentArea.setAdapter(tabPagerAdapter);
+
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                fragmentArea.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+        fragmentArea.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                tabLayout.getTabAt(position).select();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
